f7c57a2b1000900a10d1b6adf5ae5843039ca7a0
Osaretin Frank <osaretin.frank10@gmail.com>
2024-11-03 00:45:12 +0100

    add update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,29 @@
+#[allow(unused_variables)]
 fn main() {
-    let s1 = String::from("Hello world");
-    takes_ownership(&s1);
+    let mut s1 = String::from("Hello world");
+    let word_ = first_word(&mut s1);
+    // takes_ownership(&s1);
+    // println!("{s1}");
+    println!("{}", word_);
+    s1.clear();
+
+    // let hello = &s1[0..5];
+    // println!("{}", hello.len());
+}
+
+#[allow(dead_code)]
+fn takes_ownership(s1: &mut String) {
     println!("{s1}");
 }
 
-fn takes_ownership(s1: &String) {
-    println!("{s1}");
+#[allow(dead_code, unused_variables)]
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
