fcc3092d6438ef2845238ab0854392712d21c8c8
Osaretin Frank <osaretin.frank10@gmail.com>
2024-11-10 14:21:41 +0100

    Re-factor Fah-Celsius code

--- fah-celsius/src/main.rs
+++ fah-celsius/src/main.rs
@@ -1,70 +1,76 @@
 use std::io::stdin;
+#[derive(Debug)]
+struct Temperature {
+    to_unit: String,
+    value: String,
+}
+
+impl Temperature {
+    fn convert_fah_cel(self) -> String {
+        if self.to_unit.trim() == "celsius" {
+            self.to_cel()
+        } else {
+            self.to_fah()
+        }
+    }
+
+    fn to_cel(self) -> String {
+        let value: f64 = f64::from(self.int_value());
+        let cel: f64 = (5.0 / 9.0) * (value - 32.0);
+        let mut cel: String = cel.to_string();
+        cel.push_str(" Cel");
+        return cel;
+    }
+
+    fn to_fah(self) -> String {
+        // Fah = 9/5(Cel) + 32
+        let value: f64 = f64::from(self.int_value());
+        let fah: f64 = ((9.0 / 5.0) * value) + 32.0;
+        let mut fah: String = fah.to_string();
+        fah.push_str(" Fah");
+        return fah;
+    }
+
+    fn int_value(self) -> u32 {
+        let value = self
+            .value
+            .trim()
+            .parse()
+            .expect("Please input a valid number");
+        value
+    }
+}
 
 fn main() {
     loop {
-        println!("Celsius <-> Fahrenheit converter");
-        let mut temp = String::new();
-        let mut to_unit = String::new();
+        let mut fah_cel = Temperature {
+            to_unit: String::new(),
+            value: String::new(),
+        };
+        println!("Enter the unit you want to convert to: (celsius or fahrenheit or quit to End: )");
+        match stdin().read_line(&mut fah_cel.to_unit) {
+            Ok(value) => {
+                println!("{}", value.to_string());
+                if String::from(value.to_string()).trim() == "quit" {
+                    break;
+                }
+                value
+            }
+            Err(_) => {
+                println!("Please input a value");
+                continue;
+            }
+        };
 
-        println!("Please input the temperature you want to convert to: ");
-        match stdin().read_line(&mut to_unit) {
+        println!("Enter the value: ");
+        match stdin().read_line(&mut fah_cel.value) {
             Ok(value) => value,
-            Err(_) => break,
+            Err(_) => {
+                println!("Please input a valid value");
+                continue;
+            }
         };
 
-        let to_unit = to_unit.trim();
-
-        if to_unit != "celsius" && to_unit != "fahrenheit" {
-            println!("Unit to convert to must be Celsius or Fahrenheit: ");
-            break;
-        }
-
-        let input_temp = if to_unit == "celsius" {
-            "Fahrenheit"
-        } else {
-            "Celsius"
-        };
-
-        println!("Please input the {} temperature value", input_temp);
-        match stdin().read_line(&mut temp) {
-            Ok(value) => value,
-            Err(_) => break,
-        };
-
-        let temp: i32 = match temp.trim().parse() {
-            Ok(value) => value,
-            Err(_) => break,
-        };
-
-        println!("{}", convert_fah_cel(to_unit, temp));
+        println!("{}", fah_cel.convert_fah_cel());
     }
-}
-
-#[allow(dead_code, unused_variables)]
-fn convert_fah_cel(to_unit: &str, value: i32) -> String {
-    if to_unit == "celsius" {
-        return to_cel(value);
-    } else {
-        return to_fah(value);
-    }
-}
-
-#[allow(dead_code, unused_variables)]
-fn to_cel(value: i32) -> String {
-    // Cel = 5/9(Fah - 32)
-    let value: f64 = f64::from(value);
-    let cel: f64 = (5.0 / 9.0) * (value - 32.0);
-    let mut cel: String = cel.to_string();
-    cel.push_str(" Cel");
-    return cel;
-}
-
-#[allow(dead_code, unused_variables)]
-fn to_fah(value: i32) -> String {
-    // Fah = 9/5(Cel) + 32
-    let value: f64 = f64::from(value);
-    let fah: f64 = ((9.0 / 5.0) * value) + 32.0;
-    let mut fah: String = fah.to_string();
-    fah.push_str(" Fah");
-    return fah;
 }
