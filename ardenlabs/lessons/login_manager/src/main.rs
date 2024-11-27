5fdd11045a71f8ff29384140ff56f11fac2a4e54
Osaretin Frank <osaretin.frank10@gmail.com>
2024-11-27 10:06:21 +0100

    add cli example --ap

--- ardenlabs/lessons/login_manager/src/main.rs
+++ ardenlabs/lessons/login_manager/src/main.rs
@@ -1,20 +1,101 @@
-use clap::Parser;
+use authentication::{add_user, delete_user, get_users, update_user};
+use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
-#[command(about, version, long_about = None)]
+#[command(version, about, long_about = None)]
 struct Args {
-    // Name to Greet
-    #[arg(short, long)]
+    /// Name of User to greet
+    #[arg(long, short, default_value_t = String::from("Frank"))]
     name: String,
-
-    // NUmber of time to greet
-    #[arg(short, long, default_value_t = 1)]
+    /// Number of times to greet
+    #[arg(long, short, default_value_t = 1)]
     count: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
+#[derive(Debug, Subcommand)]
+enum Commands {
+    ///List all users
+    List,
+
+    /// Add a user
+    Add {
+        /// User username
+        username: String,
+
+        /// User password
+        password: String,
+
+        /// Optional - Is user an admin
+        #[arg(long, short)]
+        admin: Option<bool>,
+    },
+
+    /// Delete a User
+    Delete {
+        /// User to delete
+        username: String,
+    },
+
+    /// Update a user
+    Update {
+        /// User to update
+        username: String,
+
+        /// New User name
+        #[arg(long)]
+        new_username: Option<String>,
+
+        /// New Password
+        #[arg(long)]
+        new_password: Option<String>,
+    },
+}
+
+fn list_users() {
+    println!("{:40} {:20}", "Username", "Role",);
+    println!("{:-<70}", "");
+    let users = get_users();
+    users.iter().for_each(|(_, user)| {
+        println!("{:40} {:20?}", user.username, user.role);
+    });
+}
 fn main() {
     let cli = Args::parse();
-    for _ in 0..cli.count {
-        println!("Hello {}", cli.name)
-    }
+    // for _ in 0..cli.count {
+    //     println!("{}", cli.name)
+    // }
+
+    match cli.command {
+        Some(Commands::List) => {
+            list_users();
+        }
+
+        #[allow(unused)]
+        Some(Commands::Add {
+            username,
+            password,
+            admin,
+        }) => {
+            add_user(username, password, admin.unwrap_or(false));
+        }
+
+        Some(Commands::Delete { username }) => {
+            delete_user(username);
+        }
+
+        Some(Commands::Update {
+            username,
+            new_username,
+            new_password,
+        }) => {
+            update_user(username, new_username, new_password);
+        }
+
+        None => {
+            println!("Run with -- --help to see instructions")
+        }
+    };
 }
